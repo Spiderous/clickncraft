004cd464c1815a172a8f1bc1f02cdb6fed03baf4
Spiderous <spiderous@protonmail.com>
2019-12-27 22:00:38 +0100

    Initialized new project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+@Component
+export default class Home extends Vue {}
 </script>
